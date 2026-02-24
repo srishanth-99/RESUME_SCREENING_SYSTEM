821e01885bbeb5e99c542d8c0bac5b97f8490fe7
BOMMEDA SRISHANTH <srishanthnani4@gmail.com>
2026-02-24 15:14:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def add_bg_image(image_path):
         unsafe_allow_html=True
     )
 
-add_bg_image("C:\\WEB dev\\OneDrive\\Desktop\\resume screening system\\assets\\bg1.png.jpg")
+add_bg_image("assets\bg1.png.jpg")
 
 # -----------------------------
 # API
@@ -75,3 +75,4 @@ if uploaded_file:
             st.error("⏳ Backend took too long to respond.")
         except Exception as e:
             st.error(f"⚠️ Unexpected error: {e}")
+
