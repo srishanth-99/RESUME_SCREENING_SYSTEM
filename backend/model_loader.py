9a147ec08217f163a67d70e282d8038c863bd015
BOMMEDA SRISHANTH <srishanthnani4@gmail.com>
2026-02-25 18:39:11 +0530

    Delete model_loader.py

--- backend/model_loader.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-model = pickle.load(open("model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
-
-def predict_role(text):
-    data = vectorizer.transform([text])
-    return model.predict(data)[0]
